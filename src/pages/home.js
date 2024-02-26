bbab2639ad062a35b15d269b603051cc01285140
Sasi <sasikaran2k3@gmail.com>
2024-02-27 00:21:30 +0530

    No duplicate and remove button

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,4 @@
-import { useEffect, useState } from "react";
-import { json } from "react-router-dom";
+import { useState } from "react";
 import Table from 'react-bootstrap/Table';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -8,6 +7,24 @@ const Home = ()=>
 {
     const [students,setStudents] = useState([]);
 
+    const ListInsert = (json_response)=>{
+        let flag = 0;
+        for(let i=0;i<students.length;i++){
+            if(students[i]["_id"] === json_response["_id"])
+            {
+                flag=1;
+                break; 
+            }
+        }
+        if(flag===0)
+            setStudents([...students, json_response]);
+        else
+            console.log("Duplicate Element");
+    }
+    const ListDelete = (index)=>{
+        students.pop(index);
+        console.log("POP");
+    }
     const fetchDetails = async (reg)=>
     {
         const response = await fetch("https://attendencesystembackend.onrender.com/search/"+reg);
@@ -16,7 +33,8 @@ const Home = ()=>
             const json_response = await response.json();
             console.log(JSON.stringify(json_response));
             //const newItem = {"_id" : "1", "name":"a","counsellor":"b","register_no":"123"};
-            setStudents([...students, json_response]);
+            //setStudents([...students, json_response]);
+            ListInsert(json_response);
             console.log("Inner "+ students);
         }
         else
@@ -54,14 +72,17 @@ const Home = ()=>
                     </thead>
                     <tbody>
                         {
-                        Array.isArray(students) && students.map((student) => (
-                                    <tr key={student.Register_no}>
+                        Array.isArray(students) && students.map((student,index) => (
+                                    <tr key={index}>
                                         <td  >{student.Register_no}</td>
                                         <td >{student["NAME"]}</td>
                                         <td >{student["D/H"]}</td>
                                         <td >{student["M/F"]}</td>
+                                        <td ><button >Remove</button></td>
                                     </tr>
+                                    
                         ))
+                        
                         }
                     </tbody>
                 </Table>
